11b55039d907dc4d4316ad3dc7ac6264c6dac21c
Juca Crispim <juca@poraodojuca.net>
2020-02-09 03:42:28 -0300

    Fix runner

--- toxicjasmine/__init__.py
+++ toxicjasmine/__init__.py
@@ -189,42 +189,9 @@ class ToxicjasmineFile(object):
         )
 
 
-class ToxicJasmineCIRunner(CIRunner):
-
-    def _start_test_server(self, app, browser, port):
-        test_server = TestServerThread(port, app=app)
-        test_server.start()
-        options = app.jasmine_config.browser_options
-        browser_opts = [o for o in options.split(',')] if options else []
-
-        driver = browser if browser \
-            else os.environ.get('JASMINE_BROWSER', 'firefox')
-        try:
-            webdriver = __import__(
-                "selenium.webdriver.{0}.webdriver".format(driver),
-                globals(), locals(), ['object'], 0
-            )
-
-            options = webdriver.Options()
-            for option in browser_opts:
-                options.add_argument('--{}'.format(option))
-
-            if driver == 'chrome':
-                kw = {'chrome_options': options}
-            elif driver == 'firefox':
-                kw = {'firefox_options': options}
-            else:
-                kw = {}
-
-            self.browser = webdriver.WebDriver(**kw)
-        except ImportError:
-            print("Browser {0} not found".format(driver))
-        return test_server
-
-
 def begin():
 
-    cmd = ToxicjasmineCommand(ToxicjasmineApp, ToxicJasmineCIRunner)
+    cmd = ToxicjasmineCommand(ToxicjasmineApp, CIRunner)
 
     if 'python' in sys.argv or 'python3' in sys.argv:
         args = sys.argv[2:]
