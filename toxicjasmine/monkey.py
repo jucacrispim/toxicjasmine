0375053f7bfd044ffe7fd9325845df80e592911a
Juca Crispim <jucacrispim@gmail.com>
2023-05-01 00:44:28 -0300

    Fix import

--- toxicjasmine/monkey.py
+++ toxicjasmine/monkey.py
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with toxicjasmine. If not, see <http://www.gnu.org/licenses/>.
 
+from yaml import load, Loader
 
 class MonkeyPatcher:
 
