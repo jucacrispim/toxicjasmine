0d9c53568b02d24c63fd4e90a5d4dee44fdea6c0
Juca Crispim <jucacrispim@gmail.com>
2024-06-15 14:41:34 -0300

    Remove firefox from ci tests

--- tests/test_ci_command.py
+++ tests/test_ci_command.py
@@ -11,7 +11,7 @@ from . import TEST_DATA_DIR
 def test_ci_with_tests_passed():
     ci = commands.CI(Mock())
     args = Mock(fpath=os.path.join(TEST_DATA_DIR, 'SpecRunner.html'))
-    args.browser = 'firefox'
+    args.browser = 'chrome'
     args.show_logs = False
     args.options = None
     r = ci.run(args)
@@ -22,7 +22,7 @@ def test_ci_with_tests_passed():
 def test_ci_with_tests_passed_with_browser_options():
     ci = commands.CI(Mock())
     args = Mock(fpath=os.path.join(TEST_DATA_DIR, 'SpecRunner.html'))
-    args.browser = 'firefox'
+    args.browser = 'chrome'
     args.show_logs = False
     args.options = 'no-sandbox,headless'
     r = ci.run(args)
@@ -34,7 +34,7 @@ def test_ci_with_tests_error(mocker):
     mocker.patch.object(commands, '_exit_with_error', Mock())
     ci = commands.CI(Mock())
     args = Mock(fpath=os.path.join(TEST_DATA_DIR, 'SpecRunnerError.html'))
-    args.browser = 'firefox'
+    args.browser = 'chrome'
     args.show_logs = False
     args.options = None
     ci.run(args)
